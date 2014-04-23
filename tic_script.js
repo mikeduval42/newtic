a2fae3994f4c2643a80ae849abb0d4b75e7d7afa
michael duval <michaelduval@michaels-MacBook-Pro-2.local>
2014-04-22 17:57:26 -0700

    first commit of files

---
+++ tic_script.js
@@ -0,0 +1,37 @@
+var gameApp = angular.module('gameApp', []);
+gameApp.controller('GameController', function($scope){
+
+  var turn = true;
+
+  $scope.playerTurn = function(r, c)
+  {
+    if(turn === true)
+    {
+      $scope.rows[r][c] = 1;  // X
+      value = 1;
+      turn =false;
+    }
+    else
+    {
+      $scope.rows[r][c] = -1; // O
+      value = -1;
+      turn = true;
+    }
+    // Test various win conditions
+    if(Math.abs($scope.rows[0][0] + $scope.rows[0][1] + $scope.rows[0][2]) == 3)
+      $scope.playerWon("top row", $scope.rows[0][2] == 3);
+    // if(Math.abs($scope.rows[0][0] + $scope.rows[1][1] + $scope.rows[2][2]) == 3)
+    //   $scope.playerWon("\\", $scope.rows[0][0] == 3);
+    if(Math.abs($scope.rows[1][0] + $scope.rows[1][1] + $scope.rows[1][2]) == 3)
+      $scope.playerWon("middle row", $scope.rows[0][1] == 3);
+    // if(Math.abs($scope.rows[2][0] + $scope.rows[2][1] + $scope.rows[2][2]) == 3)
+    //   $scope.playerWon("bottom", $scope.rows[0][1] == 3);
+  };
+
+  // Show an alert if one of the tested directions is a win
+  $scope.playerWon = function(descrip, player)
+  {
+    // Some example text that could be shown: "X won by diagonal"
+    alert( ((player == 1)?"X" : "O") + " won by " + descrip);
+  }
+});
